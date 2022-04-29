4ca2dd518e5a395188a4c56da07bd69e3884285c
Marshal <ga031243@gmail.com>
2022-04-29 11:19:24 +0800

    problem solved

--- src/com/fju/ai/Box2.java
+++ src/com/fju/ai/Box2.java
@@ -1,7 +1,13 @@
 package com.fju.ai;
 
 public class Box2 extends Box{
-    public Box2(){
+    public Box2() {
         super(20, 20, 20);
     }
+        @Override
+        public String toString() {
+            return "no2";
+    }
 }
+
+
